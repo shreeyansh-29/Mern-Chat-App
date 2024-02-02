6b2e31140631c25c1b301adde979d34d9e1c2c93
shreeyansh-29 <shreeyansh.rawat@gmail.com>
2024-02-02 23:33:25 +0530

    fixed socket connection

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,10 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://instabook-mtij.onrender.com"
+        : "http://localhost:3000",
   },
 });
 
